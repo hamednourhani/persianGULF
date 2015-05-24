dd8b2527284cc0ff329798a4ddbf44322ea72340
hamednourhani <info@itstar.ir>
2015-05-24 09:39:12 +0430

    author page permalink fixed

--- php-archive/searchform.php
+++
@@ -1,8 +0,0 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div>
-        <label for="s" class="screen-reader-text"><?php _e('Search for:','persianGULF'); ?></label>
-        <input type="search" id="s" name="s" value="" />
-
-        <button type="submit" id="searchsubmit" ><?php _e('Search','persianGULF'); ?></button>
-    </div>
-</form>
